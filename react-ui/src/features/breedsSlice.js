931bdad1fd77a8438986a6ca3efd20eab4620ee0
Luis Martinez <rickymtz1999@gmail.com>
2023-05-23 18:37:10 -0500

    call dog ceo api and handle the response

--- react-ui/src/features/breedsSlice.js
+++ react-ui/src/features/breedsSlice.js
@@ -1,9 +1,21 @@
 import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
 
+export const fetchBreeds = createAsyncThunk('breeds/all', async() => {
+  const response = await fetch('https://dog.ceo/api/breeds/list/all');
+  const data = await response.json();
+  const breeds = Object.keys(data.message);
+  return breeds;
+});
+
 export const breedsSlice = createSlice({
   name: 'breeds',
   initialState: [],
-  reducers: {}
+  reducers: {},
+  extraReducers(builder)  {
+  builder.addCase(fetchBreeds.fulfilled, (state,action) => {
+    return action.payload;
+  });
+ } 
 });
 
 // allows us to read from this slice of state
