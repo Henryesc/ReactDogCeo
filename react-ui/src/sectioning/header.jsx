913fb2a28065baaf4884ccffd4f4f6ba1cab551f
Luis Martinez <rickymtz1999@gmail.com>
2023-05-23 18:14:31 -0500

    adds git ignore

--- react-ui/src/sectioning/header.jsx
+++ react-ui/src/sectioning/header.jsx
@@ -4,11 +4,11 @@ import data from '../mocks/navs.json';
 
 const Header = () => {
 
-  const navs = data.map(nav => <NavLink to={nav.href}>{nav.name}</NavLink>);
+  const navs = data.map((nav,index) => <NavLink to={nav.href} key={`${nav.name}-${index}`}> {nav.name} </NavLink>);
 
   return (
     <header>
-      <div className="y-navs y-wrap">{navs}</div>
+       <div className="y-wrap y-navs"> <img className="image" src= "https://p7.hiclipart.com/preview/760/225/292/logo-silhouette-dog-bone-dog.jpg" /> {navs} </div> 
     </header>
   );
 };
