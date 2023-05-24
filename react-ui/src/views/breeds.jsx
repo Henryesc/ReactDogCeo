1416c294a81bbfb9a59f903198e34118915b132e
Luis Martinez <rickymtz1999@gmail.com>
2023-05-23 19:04:57 -0500

    shows breeds

--- react-ui/src/views/breeds.jsx
+++ react-ui/src/views/breeds.jsx
@@ -1,8 +1,20 @@
 import React from 'react';
+import { selectBreeds } from '../features/breedsSlice';
+import { useSelector } from 'react-redux';
 
 const Breeds = () => {
+
+  const data = useSelector(selectBreeds);
+
+  const breeds = data && data.map (breed => {
+    return <li>{breed}</li>;
+    })
+
   return (
-    <h1>Breeds</h1>
+    <>
+      <h1>Breeds</h1>
+      <ul>{breeds}</ul>
+    </>
   )
 };
 
